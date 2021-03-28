658a2d16ac8713dc74721e799ebdbad2e0f34d45
marcus0226 <bmarcusv15@yahoo.com>
2021-03-28 07:12:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    input.addEventListener('click', clickAlert());
 }
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
